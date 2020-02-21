bbae45ac2f044abe2d97cce7976692c5c37243e6
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-02-21 17:51:45 -0500

    Done.

--- lib/post.rb
+++ lib/post.rb
@@ -18,8 +18,8 @@ class Post
 	end
 
 	def author_name
-		self.author ? self.author.name : nil
-		# self.author.name ?: nil
+		# self.author ? self.author.name : nil
+		self.author.name ?: nil
 	end
 
 	def self.all
